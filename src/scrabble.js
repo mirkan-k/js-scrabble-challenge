01f346a2137ef2259ca96a8fc9ccb980aeef251d
mirkan-k <mirkilinc@gmail.com>
2022-05-25 17:36:48 +0100

    specComplete

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,53 @@
 class Scrabble {
   // Write your implementation here
+  constructor(wordEntry) {
+    this.wordEntry = wordEntry
+  }
 
+  score() {
+    let wordScore = 0
+    if (this.wordEntry === null) {
+      return wordScore
+    }
+
+    const letterValue = {
+      a: 1,
+      b: 3,
+      c: 3,
+      d: 2,
+      e: 1,
+      f: 4,
+      g: 2,
+      h: 4,
+      i: 1,
+      j: 8,
+      k: 5,
+      l: 1,
+      m: 3,
+      n: 1,
+      o: 1,
+      p: 3,
+      q: 10,
+      r: 1,
+      s: 1,
+      t: 1,
+      u: 1,
+      v: 4,
+      w: 4,
+      x: 8,
+      y: 4,
+      z: 10
+    }
+
+    for (let i = 0; i < this.wordEntry.length; i++) {
+      wordScore += letterValue[this.wordEntry.toLowerCase()[i]] || 0
+    }
+
+    return wordScore
+  }
 }
+const scrabble = new Scrabble('Quiz')
+
+console.log(scrabble.score())
 
 module.exports = Scrabble
