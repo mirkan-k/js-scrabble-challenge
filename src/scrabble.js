d70f455bba5550d8be08cff9fd28ae4b1be923cb
mirkan-k <mirkilinc@gmail.com>
2022-05-27 11:50:20 +0100

    specComplete + Extensions

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,64 +1,82 @@
+const letterValue = {
+  a: 1,
+  b: 3,
+  c: 3,
+  d: 2,
+  e: 1,
+  f: 4,
+  g: 2,
+  h: 4,
+  i: 1,
+  j: 8,
+  k: 5,
+  l: 1,
+  m: 3,
+  n: 1,
+  o: 1,
+  p: 3,
+  q: 10,
+  r: 1,
+  s: 1,
+  t: 1,
+  u: 1,
+  v: 4,
+  w: 4,
+  x: 8,
+  y: 4,
+  z: 10
+}
+
 class Scrabble {
   // Write your implementation here
   constructor(wordEntry) {
     this.wordEntry = wordEntry
   }
 
+  getWordMultiplier() {
+    /* I had to change the Brackets for a different symbol because I ran into the problem where if the first and last letters are double or triple, the whole word counts as double/triple. */
+    const doubleWordCheck =
+      this.wordEntry[0] === '+' &&
+      this.wordEntry[this.wordEntry.length - 1] === '+'
+    const tripleWordCheck =
+      this.wordEntry[0] === '*' &&
+      this.wordEntry[this.wordEntry.length - 1] === '*'
+
+    if (doubleWordCheck) {
+      return 2
+    } else if (tripleWordCheck) {
+      return 3
+    }
+    return 1
+  }
+
   score() {
     let wordScore = 0
-    // const doubleLetterCheck = /\{([^}]+)\}/;
-    // let doubleLetterMatch;
-    // let tripleLetter = /\[([^]]+)\]/;
 
     if (this.wordEntry === null) {
       return wordScore
     }
 
-    const letterValue = {
-      a: 1,
-      b: 3,
-      c: 3,
-      d: 2,
-      e: 1,
-      f: 4,
-      g: 2,
-      h: 4,
-      i: 1,
-      j: 8,
-      k: 5,
-      l: 1,
-      m: 3,
-      n: 1,
-      o: 1,
-      p: 3,
-      q: 10,
-      r: 1,
-      s: 1,
-      t: 1,
-      u: 1,
-      v: 4,
-      w: 4,
-      x: 8,
-      y: 4,
-      z: 10
-    }
-    // console.log(doubleLetterMatch)
+    const wordCheck = this.wordEntry.toLowerCase()
 
-    for (let i = 0; i < this.wordEntry.length; i++) {
-      /* TODO add double word and tripple word feature while also not running into the problem where if the first and last letters are double, it does't count the whole word as double... */
-      if (this.wordEntry[i] === '{' && this.wordEntry[i + 2] === '}') {
-        wordScore += letterValue[this.wordEntry.toLowerCase()[i + 1]] || 0
-      } else if (this.wordEntry[i] === '[' && this.wordEntry[i + 2] === ']') {
-        wordScore += letterValue[this.wordEntry.toLowerCase()[i + 1]] * 2 || 0
+    for (let i = 0; i < wordCheck.length; i++) {
+      const doubleLetterCheck = wordCheck[i] === '{' && wordCheck[i + 2] === '}'
+      const tripleLetterCheck = wordCheck[i] === '[' && wordCheck[i + 2] === ']'
+
+      if (doubleLetterCheck) {
+        wordScore += letterValue[wordCheck[i + 1]] || 0
+      } else if (tripleLetterCheck) {
+        wordScore += letterValue[wordCheck[i + 1]] * 2 || 0
       }
-      wordScore += letterValue[this.wordEntry.toLowerCase()[i]] || 0
+      wordScore += letterValue[wordCheck[i]] || 0
     }
 
-    return wordScore
+    return wordScore * this.getWordMultiplier()
   }
 }
-const scrabble = new Scrabble('Q{U}[i]z')
+const scrabble = new Scrabble('+{Q}ui{z}+')
 
 console.log(scrabble.score())
+console.log('The Word Multiplier is:', scrabble.getWordMultiplier())
 
 module.exports = Scrabble
